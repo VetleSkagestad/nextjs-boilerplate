24d0d7a42c82f4d52c7fcfff09dbcde3443ab83c
vetskage <vetle.skagestad@inmeta.no>
2025-06-20 14:20:07 +0200

    removed more stuff

--- app/homepage/homepage.tsx
+++ app/homepage/homepage.tsx
@@ -43,7 +43,7 @@ export default function Homepage() {
         </li>
           </ol> */}
 
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
+          {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
         <a
           className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -67,9 +67,9 @@ export default function Homepage() {
         >
           Read our docs
         </a>
-          </div>
+          </div> */}
         </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
           <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -115,7 +115,7 @@ export default function Homepage() {
         />
         Go to nextjs.org →
           </a>
-        </footer>
+        </footer> */}
       </div>
     </div>
   );
